fff276cb3fa958deff70293cb4d7b1975b28d2af
delumen-ivan <ivandelumen05@gmail.com>
2025-05-21 00:13:08 +0800

    second commit

--- src/test.java
+++ src/test.java
@@ -3,6 +3,6 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("gagana na kaya??");
+		System.out.println("tanginamo");
 	}
-
 }
